ecde200911408a7bcd5f58b5994841cece4c600e
Freelancer Ahsan <81156921+Ahsan5313@users.noreply.github.com>
2021-07-18 16:37:42 +0600

    Create parogram1.py, area.py

---
+++ parogram1.py, area.py
@@ -0,0 +1,14 @@
+def triangle_area(b, h):
+    print(f"Area of Triangle: {0.5*b*h}")
+
+def reactangle_area(a,b):
+    print(f"Area of reactangle : {a*b}")
+    
+    
+    
+    
+    from  area import reactangle_area, triangle_area
+
+
+triangle_area(10, 20)
+reactangle_area(10, 20)
